efa18b82f9498d9db811a9e0205807ae5a9068c7
Shicheng Guo <Shicheng.Guo@hotmail.com>
2020-01-05 22:00:29 -0600

    Update OR-HR-merge.R

--- bin/OR-HR-merge.R
+++ bin/OR-HR-merge.R
@@ -14,8 +14,8 @@ pick<-subset(out,pval<10^-3 & pval.fixed<0.05 & pval.random<0.05 & beta*(TE.fixe
 
 dim(pick)
 
-write.csv(out,file=paste(memo,".tcga.pancancer.chol.overall.pick.rnaseq.dmg.smd",".os.hr.csv",sep=""),quote=F)
-write.table(out,file=paste(memo,".tcga.pancancer.chol.overall.pick.rnaseq.dmg.smd",".os.hr.txt",sep=""),quote=F,col.names=NA,row.names=T,sep="\t")
+write.csv(out,file=paste(memo,".tcga.pancancer.chol.overall.rnaseq.dmg.smd",".os.hr.csv",sep=""),quote=F)
+write.table(out,file=paste(memo,".tcga.pancancer.chol.overall.rnaseq.dmg.smd",".os.hr.txt",sep=""),quote=F,col.names=NA,row.names=T,sep="\t")
 
 write.csv(pick,file=paste(memo,".tcga.pancancer.chol.pick.rnaseq.dmg.smd","os.hr.csv",sep=""),quote=F)
 write.table(pick,file=paste(memo,".tcga.pancancer.chol.pick.rnaseq.dmg.smd","os.hr.txt",sep=""),quote=F,col.names=NA,row.names=T,sep="\t")
@@ -48,4 +48,9 @@ write.table(cminput,file=paste(memo,".pval.fix.manhattan.qqplot.meta.OS.HR.txt",
 cminput<-data.frame(SNP=output$V5,Chromosome=chr2num(output$V1),Position=output$V2,trait1=output$pval.random)
 CMplot(cminput,plot.type="b",memo=paste(memo,".random",sep=""),LOG10=TRUE,threshold=NULL,file="jpg",dpi=300,file.output=TRUE,verbose=TRUE,width=14,height=6)
 write.table(cminput,file=paste(memo,".pval.random.manhattan.qqplot.meta.OS.HR.txt",sep=""),sep="\t",quote=F,row.name=T,col.names=NA)
+
+
+
+
+
 
